6629e79044183acd95e37cd371b1f571446dd26c
Nathan Hankinson <heyamate@gmail.com>
2013-05-12 17:44:59 +0800

    add user model with devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Onemonthrails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #So Devise will work on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
